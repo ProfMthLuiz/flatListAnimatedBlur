81cb0d2d6e1c744b53282fac192c193a5feea0ee
ProfMthLuiz <mthluiz99@gmail.com>
2024-09-01 11:04:02 -0300

    Update

--- App.js
+++ App.js
@@ -1,11 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import {
+  StyleSheet,
+  FlatList,
+  Image,
+  Animated,
+  Dimensions,
+  Text,
+  View,
+} from "react-native";
+
+const { width, height } = Dimensions.get("screen");
+const imageW = width * 0.7;
+const imageH = imageW * 1.54;
+
+const data = [
+  require("./src/assets/images/image_1.jpg"),
+  require("./src/assets/images/image_2.jpg"),
+  require("./src/assets/images/image_3.jpg"),
+  require("./src/assets/images/image_4.jpg"),
+  require("./src/assets/images/image_5.jpg"),
+  require("./src/assets/images/image_6.jpg"),
+  require("./src/assets/images/image_7.jpg"),
+  require("./src/assets/images/image_8.jpg"),
+];
 
 export default function App() {
+  const scrollX = React.useRef(new Animated.Value(0)).current;
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={StyleSheet.absoluteFillObject}>
+        {data.map((image, index) => {
+          const inputRange = [
+            (index - 1) * width,
+            index * width,
+            (index + 1) * width,
+          ];
+
+          const opacity = scrollX.interpolate({
+            inputRange,
+            outputRange: [0, 1, 0],
+          });
+
+          return (
+            <Animated.Image
+              key={`image-${index}`}
+              source={image}
+              blurRadius={5}
+              style={[StyleSheet.absoluteFillObject, { opacity }]}
+            />
+          );
+        })}
+      </View>
+      <Animated.FlatList
+        data={data}
+        keyExtractor={(_, index) => index.toString()}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: true }
+        )}
+        horizontal
+        pagingEnabled
+        renderItem={({ item }) => {
+          return (
+            <View
+              style={{
+                width,
+                justifyContent: "center",
+                alignItems: "center",
+                shadowColor: "#ff0000",
+                shadowOpacity: 0.5,
+                shadowOffset: { width: 0, height: 2 },
+                shadowRadius: 20,
+                elevation: 10,
+              }}
+            >
+              <Image
+                source={item}
+                style={{
+                  borderRadius: 16,
+                  width: imageW,
+                  height: imageH,
+                  resizeMode: "cover",
+                }}
+              />
+            </View>
+          );
+        }}
+      />
     </View>
   );
 }
@@ -13,8 +94,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
